8680d0951ae249900f1a43765fab51a572d4e1c1
Pavlo Tereshko <pavlo.tereshko@gmail.com>
2024-03-06 22:14:50 +0200

    Review fixes

--- src/homework/4.tsx
+++ src/homework/4.tsx
@@ -4,7 +4,7 @@ import noop from "lodash/noop";
 type MenuIds = "first" | "second" | "last";
 type Menu = { id: MenuIds; title: string };
 type SelectedMenu = {
-  id?: MenuIds;
+  id: MenuIds;
 };
 type MenuSelected = {
   selectedMenu: SelectedMenu;
@@ -17,7 +17,7 @@ type MenuAction = {
 // Додати тип Menu Selected
 
 const MenuSelectedContext = createContext<MenuSelected>({
-  selectedMenu: {},
+  selectedMenu: {} as SelectedMenu,
 });
 
 // Додайте тип MenuAction
@@ -32,7 +32,7 @@ type PropsProvider = {
 
 function MenuProvider({ children }: PropsProvider) {
   // Додати тип для SelectedMenu він повинен містити { id }
-  const [selectedMenu, setSelectedMenu] = useState<SelectedMenu>({});
+  const [selectedMenu, setSelectedMenu] = useState<SelectedMenu>({} as SelectedMenu);
 
   const menuContextAction = useMemo(
     () => ({
